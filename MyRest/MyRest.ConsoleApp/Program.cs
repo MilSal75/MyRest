fe9cecf4d4ee09199629e5759bf7ee8b9fcc33a7
MilSal75 <3003milana3003@gmail.com>
2026-04-21 14:29:41 +0700

    Add DB tests to Program

--- MyRest/MyRest.ConsoleApp/Program.cs
+++ MyRest/MyRest.ConsoleApp/Program.cs
@@ -1,21 +1,26 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using MyRest.Domain.Entities;
 using MyRest.Domain.ValueObjects;
 using MyRest.Domain.Exceptions;
 using VOExceptions = MyRest.Domain.ValueObjects.Exceptions;
+using MyRest.Infrastructure;
+using MyRest.Infrastructure.RepositoriesEF;
 
 namespace MyRest.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Изменила 'void' на 'async Task' для бд
+        static async Task Main(string[] args)
         {
             Console.WriteLine("ТЕСТИРОВАНИЕ БИЗНЕС-ПРАВИЛ MY REST\n");
 
             try
             {
                 // БЛОК 1: ИНИЦИАЛИЗАЦИЯ (УСПЕШНЫЙ СЦЕНАРИЙ)
-
                 Console.WriteLine(">>> БЛОК 1: Создание базы (2 менеджера, 2 сотрудника)...");
 
                 var managerIvan = new Manager(new PersonName("Иван"), new PersonName("Иванов"), new PhoneNumber("+79001111111"));
@@ -26,9 +31,7 @@ namespace MyRest.ConsoleApp
 
                 Console.WriteLine("Сотрудники успешно наняты своими менеджерами.\n");
 
-               
                 // БЛОК 2: ПРОВЕРКА ПРАВ ДОСТУПА (СВОЙ/ЧУЖОЙ)
-                
                 Console.WriteLine(">>> БЛОК 2: Тестирование изоляции подразделений...");
 
                 TestRule("Менеджер Иван пытается уволить чужого сотрудника (Олега)", () =>
@@ -56,9 +59,7 @@ namespace MyRest.ConsoleApp
                     empAnna.MarkAttendance(assignmentOleg);
                 });
 
-                
                 // БЛОК 3: ПРОВЕРКА БИЗНЕС-ЛОГИКИ
-                
                 Console.WriteLine(">>> БЛОК 3: Тестирование логики объектов...");
 
                 TestRule("Менеджер создает смену длительностью 13 часов (максимум 12)", () =>
@@ -82,7 +83,6 @@ namespace MyRest.ConsoleApp
                 });
 
                 // БЛОК 4: ПРОВЕРКА СОСТОЯНИЙ (СТАТУС УВОЛЕННОГО)
-                
                 Console.WriteLine(">>> БЛОК 4: Тестирование жизненного цикла (увольнение)...");
 
                 // Увольняем Олега легально
@@ -111,9 +111,7 @@ namespace MyRest.ConsoleApp
                 Console.WriteLine($"[КРИТИЧЕСКАЯ СИСТЕМНАЯ ОШИБКА]: {ex.Message}");
             }
 
-            
             // БЛОК 5: ПРОВЕРКА ВАЛИДАТОРОВ (VALUE OBJECTS)
-            
             Console.WriteLine(">>> БЛОК 5: Тестирование входных данных...");
 
             TestValidationRule("Попытка нанять сотрудника с цифрами в имени (Олег123)", () =>
@@ -121,7 +119,54 @@ namespace MyRest.ConsoleApp
                 var badName = new PersonName("Олег123");
             });
 
-            Console.WriteLine("\nТЕСТИРОВАНИЕ УСПЕШНО ЗАВЕРШЕНО");
+            // НОВЫЙ БЛОК: ТЕСТ РАБОТЫ С БД
+
+            Console.WriteLine("\n>>> БЛОК 6: Тест работы с базой данных (EF Core)...");
+            try
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyRestDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+                using var context = new ApplicationDbContext(optionsBuilder.Options);
+                var managerRepo = new ManagerRepository(context);
+                var employeeRepo = new EmployeeRepository(context);
+
+                Console.WriteLine("[-] Подготовка чистой базы данных...");
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.MigrateAsync();
+
+                Console.WriteLine("[-] Создаем новые объекты для сохранения...");
+                var dbManager = new Manager(
+                    new PersonName("Анастасия"),
+                    new PersonName("Скороходова"),
+                    new PhoneNumber("+79001112233"));
+
+                var dbEmployee = dbManager.HireEmployee(
+                    new PersonName("Иван"),
+                    new PersonName("Студентов"),
+                    new PhoneNumber("+79009998877"));
+
+                Console.WriteLine("[-] Сохраняем объекты в SQL-базу...");
+                await managerRepo.AddAsync(dbManager);
+                await employeeRepo.AddAsync(dbEmployee);
+
+                Console.WriteLine("[-] Читаем сохраненные данные из базы...");
+                var savedManagers = await managerRepo.GetAllAsync();
+                var loadedManager = savedManagers.First();
+
+                var savedEmployees = await employeeRepo.GetAllAsync();
+                var loadedEmployee = savedEmployees.First();
+
+                Console.WriteLine($"\nУСПЕХ! Менеджер из БД: {loadedManager.FirstName} {loadedManager.LastName}");
+                Console.WriteLine($"УСПЕХ! Сотрудник из БД: {loadedEmployee.FirstName} {loadedEmployee.LastName}");
+                Console.WriteLine($"УСПЕХ! Связь установлена: {loadedEmployee.ManagerId == loadedManager.Id}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\n[ОШИБКА БАЗЫ ДАННЫХ]: {ex.Message}");
+            }
+
+            Console.WriteLine("\nТЕСТИРОВАНИЕ ПОЛНОСТЬЮ ЗАВЕРШЕНО. Нажмите Enter для выхода...");
             Console.ReadLine();
         }
 
